7837ce7f6acf8f72b07bbf13754b353beefa7e67
Olliehub <charlibossubom@gmail.com>
2024-06-28 23:05:09 +0000

    update place.py

--- models/place.py
+++ models/place.py
@@ -7,13 +7,14 @@ from os import getenv
 from models.review import Review
 from sqlalchemy.orm import relationship
 
+
 STORAGE = getenv("HBNB_TYPE_STORAGE")
 
 
 class Place(BaseModel, Base):
-""" A place to stay """
-__tablename__ = 'places'
-if STORAGE == "db":
+    """ A place to stay """
+    __tablename__ = 'places'
+    if STORAGE == "db":
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
